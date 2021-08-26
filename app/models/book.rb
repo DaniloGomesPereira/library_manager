456e93c743c71ba8e30f436c3bdf8e56b17c8793
DaniloGomesPereira <danilogope@outlook.com>
2021-08-26 20:09:11 -0300

    fix associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
 	belongs_to :publisher
+
+	has_many :author_books, dependent: :destroy
+	has_many :authors, through: :author_books
 end
---
+++ db/migrate/20210826225618_create_author_books.rb
@@ -0,0 +1,11 @@
+class CreateAuthorBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :author_books do |t|
+      t.references :author, null: false, foreign_key: true
+      t.references :book, null: false, foreign_key: true
+      t.boolean :main_author, default: true
+
+      t.timestamps
+    end
+  end
+end
