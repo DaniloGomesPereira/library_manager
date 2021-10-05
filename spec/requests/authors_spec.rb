4d5048dd6955bc95c72402fffc87f19b231422a5
DaniloGomesPereira <danilogope@outlook.com>
2021-10-05 07:48:21 -0300

    fixing rubocop's indentation offences

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -3,102 +3,103 @@
 require 'rails_helper'
 
 RSpec.describe 'Authors', type: :request do
-    describe '#index' do
-        subject(:index_authors) { get '/authors' }
+  describe '#index' do
+    subject(:index_authors) { get '/authors' }
 
-        it { is_expected.to eq(200) }
+    it { is_expected.to eq(200) }
+  end
+
+  describe '#show' do
+    subject(:show_authors) { get "/authors/#{author_id}" }
+
+    context 'when author exists' do
+      let!(:author_id) { create(:author).id }
+
+      it { is_expected.to eq(200) }
     end
 
-    describe '#show' do
-        subject(:show_authors) { get "/authors/#{author_id}" }
+    context 'when author does not exists' do
+      let(:author_id) { -1 }
 
-        context 'when author exists' do
-            let!(:author_id) { create(:author).id }
+      it 'then raise record not found' do
+        expect { show_authors }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 
-            it { is_expected.to eq(200) }
-        end
+  describe '#create' do
+    subject(:post_authors) { post '/authors', params: params.as_json }
 
-        context 'when author does not exists' do
-            let(:author_id) { -1 }
-
-            it 'then raise record not found' do
-                expect { show_authors }.to raise_error(ActiveRecord::RecordNotFound)
-            end
-        end
+    let(:params) do
+      {
+        author: {
+          name: 'Diego da Silva',
+          nationality: 'Brazilian',
+          birthday: '2000-03-12'
+          }
+        }
     end
 
-    describe '#create' do
-        subject(:post_authors) { post '/authors', params: params.as_json }
-
-        let(:params) do
-            {
-                author: {
-                    name: 'Diego da Silva',
-                    nationality: 'Brazilian',
-                    birthday: '2000-03-12'
-                }
-            }
-        end
-
-        context 'with valid arguments' do
-            it 'then save the author' do
-                expect { post_authors }.to change(Author, :count).by(1)
-            end
-        end
-
-        context 'with invalid arguments' do 
-            let(:params) do
-                {
-                    author: {
-                        name: '',
-                        nationality: 'Brazilian',
-                        birthday: '2000-03-12'
-                    }
-                }
-            end
-            it 'then should not create a new record' do
-               expect { post_authors }.not_to change(Author, :count)
-            end
-        end    
+    context 'with valid arguments' do
+      it 'then save the author' do
+        expect { post_authors }.to change(Author, :count).by(1)
+      end
     end
+
+    context 'with invalid arguments' do 
+      let(:params) do
+        {
+            author: {
+            name: '',
+            nationality: 'Brazilian',
+            birthday: '2000-03-12'
+                      }
+          }
+      end
+
+      it 'then should not create a new record' do
+        expect { post_authors }.not_to change(Author, :count)
+      end
+    end    
+  end
     
-    describe '#update' do
-        subject(:put_authors) { put "/authors/#{author.id}", params: params.as_json }
+  describe '#update' do
+    subject(:put_authors) { put "/authors/#{author.id}", params: params.as_json }
 
-        let!(:author) { create(:author) }
+    let!(:author) { create(:author) }
 
-        context 'With valid arguments' do
-            let(:params) { { author: { name: 'Diego' } } }
+    context 'With valid arguments' do
+      let(:params) { { author: { name: 'Diego' } } }
 
-            it "Then updates the author's name" do
-                expect { put_authors }.to change{ author.reload.name }
-            end
-        end
+      it "Then updates the author's name" do
+        expect { put_authors }.to change{ author.reload.name }
+      end
+    end
 
-        context 'With invalid arguments' do
-            let(:params) { { author: { name: '' } } } 
+    context 'With invalid arguments' do
+      let(:params) { { author: { name: '' } } } 
               
-            it "Then should not update the author's name " do
-                expect { put_authors }.not_to change{ author.reload.name }
-            end
-        end
+      it "Then should not update the author's name " do
+        expect { put_authors }.not_to change{ author.reload.name }
+      end
     end
+  end
     
-    describe '#destroy' do 
-        subject(:delete_authors) { delete "/authors/#{author_id}" }
+  describe '#destroy' do 
+    subject(:delete_authors) { delete "/authors/#{author_id}" }
 
-        let!(:author_id) { create(:author).id }
+    let!(:author_id) { create(:author).id }
 
-        context 'with valid arguments' do
-            it { expect { delete_authors }.to change(Author, :count).by(-1) }
-        end
-
-        context 'with invalid arguments' do
-          let!(:author_id) { -1 }
-
-            it 'then should not destroy the author' do
-                expect { delete_authors }.to raise_error(ActiveRecord::RecordNotFound)
-            end
-        end            
+    context 'with valid arguments' do
+      it { expect { delete_authors }.to change(Author, :count).by(-1) }
     end
+
+    context 'with invalid arguments' do
+      let!(:author_id) { -1 }
+
+      it 'then should not destroy the author' do
+        expect { delete_authors }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end            
+  end
 end
--- spec/requests/publishers_spec.rb
+++ spec/requests/publishers_spec.rb
@@ -6,97 +6,98 @@ RSpec.describe 'Publishers', type: :request do
   describe '#index' do
     subject (:index_publisher) { get '/publishers' }
 
-      it { is_expected.to eq(200) }
+    it { is_expected.to eq(200) }
   end
 
-    describe '#show' do
-      subject (:show_publishers) { get "/publishers/#{publisher_id}" }
+  describe '#show' do
+    subject (:show_publishers) { get "/publishers/#{publisher_id}" }
 
-        context 'When Publisher exist' do
-          let!(:publisher_id) { create(:publisher).id }
+    context 'When Publisher exist' do
+      let!(:publisher_id) { create(:publisher).id }
 
-          it { is_expected.to eq(200)}
-        end
-
-        context 'When publisher does not exists' do
-          let(:publisher_id) { -1 }
-
-            it 'Then raise record not found' do
-              expect { show_publishers }.to raise_error(ActiveRecord::RecordNotFound)
-            end
-        end
+      it { is_expected.to eq(200)}
     end
 
-    describe '#create' do
-      subject (:post_publishers) { post '/publishers', params: params.as_json }
+    context 'When publisher does not exists' do
+      let(:publisher_id) { -1 }
 
-        let(:params) do
-        {
-            publisher: {
-                name: 'Editora Divertida', 
-                fundation: '1987-01-10', 
-                country: 'Brazil'
-              }
+      it 'Then raise record not found' do
+        expect { show_publishers }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe '#create' do
+    subject (:post_publishers) { post '/publishers', params: params.as_json }
+
+    let(:params) do
+      {
+        publisher: {
+        name: 'Editora Divertida', 
+        fundation: '1987-01-10', 
+        country: 'Brazil'
           }
-      end
-
-        context 'When valid arguments' do
-          it 'Then save the Publisher' do
-              expect { post_publishers }.to change(Publisher, :count).by(1)
-            end
-        end
-
-        context 'When invalid arguments' do
-        let(:params) do
-          {
-                publisher: {
-                    name: '', 
-                    fundation: '1987-01-10', 
-                    country: 'Brazil'
-                }
-            }
-        end
-        it 'then should not create a new record' do
-          expect { post_publishers }.not_to change(Publisher, :count)
-        end
-      end
-
-    describe '#update'
-        subject(:put_publishers) { put "/publishers/#{publisher.id}", params: params.as_json }
-        let!(:publisher) { create(:publisher) }
-
-        context 'Whit valid arguments' do
-          let(:params) { { publisher: { name: 'Editora Divertida' } } }
-
-            it "Then update publisher's name" do
-              expect { put_publishers }.to change { publisher.reload.name }
-            end
-        end
-
-        context 'With invalid arguments' do
-          let(:params) { { publisher: { name: '' } } }
-
-            it "Then should not update publisher's name" do
-              expect { put_publishers }.not_to change { publisher.reload.name }
-            end
-        end
+        }
     end
 
-    describe '#destroy' do
-      subject(:delete_publishers) { delete "/publishers/#{publisher_id}" }
-        
-        let!(:publisher_id) { create(:publisher).id }
-
-        context 'With valid arguments' do
-          it { expect { delete_publishers }.to change(Publisher, :count).by(-1) }
-        end
-
-        context 'With invalid arguments' do
-          let!(:publisher_id) { -1 }
-
-            it 'Then should not destroy the publisher' do
-              expect { delete_publishers }.to raise_error(ActiveRecord::RecordNotFound)
-            end
-        end
+    context 'When valid arguments' do
+      it 'Then save the Publisher' do
+        expect { post_publishers }.to change(Publisher, :count).by(1)
+      end
     end
+
+    context 'When invalid arguments' do
+    let(:params) do
+      {
+        publisher: {
+        name: '', 
+        fundation: '1987-01-10', 
+        country: 'Brazil'
+          }
+        }
+    end
+    it 'then should not create a new record' do
+      expect { post_publishers }.not_to change(Publisher, :count)
+    end
+  end
+
+  describe '#update'
+    subject(:put_publishers) { put "/publishers/#{publisher.id}", params: params.as_json }
+
+    let!(:publisher) { create(:publisher) }
+
+    context 'Whit valid arguments' do
+      let(:params) { { publisher: { name: 'Editora Divertida' } } }
+
+      it "Then update publisher's name" do
+        expect { put_publishers }.to change { publisher.reload.name }
+      end
+    end
+
+    context 'With invalid arguments' do
+      let(:params) { { publisher: { name: '' } } }
+
+      it "Then should not update publisher's name" do
+        expect { put_publishers }.not_to change { publisher.reload.name }
+      end
+    end
+  end
+
+  describe '#destroy' do
+    subject(:delete_publishers) { delete "/publishers/#{publisher_id}" }
+
+    let!(:publisher_id) { create(:publisher).id }
+
+    context 'With valid arguments' do
+      it { expect { delete_publishers }.to change(Publisher, :count).by(-1) }
+    end
+
+    context 'With invalid arguments' do
+      let!(:publisher_id) { -1 }
+
+      it 'Then should not destroy the publisher' do
+        expect { delete_publishers }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
