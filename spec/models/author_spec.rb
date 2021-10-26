c49bac9c0e3f48d083c35ab6169a24d00d18f9ff
DaniloGomesPereira <danilogope@outlook.com>
2021-10-26 09:58:27 -0300

    adjusting shoulda-matcher's tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -14,11 +14,12 @@ RSpec.describe Author do
 
     context "when it's not valid" do
       it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:nationality) }
+      it { should validate_presence_of(:birthday) }
       # let(:author) { build(:author, name: nil) }
 
       # it 'then not OK' do
-      #   expect(author).not_to be_valid
-      
+      #   expect(author).not_to be_valid      
     end
   end
 end
