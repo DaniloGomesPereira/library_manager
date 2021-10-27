07fc51d2bb9afd4bf8650df2754398caef1972f4
DaniloGomesPereira <danilogope@outlook.com>
2021-10-27 08:33:58 -0300

    confirmar alterações

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,23 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Author do
-  describe 'Given an author' do
-    context "when it's valid" do
-      let(:author) { build(:author) }
-
-      it 'then OK' do
-        expect(author).to be_valid
-      end
-    end
-
-    context "when it's not valid" do
-      it { should validate_presence_of(:name) }
-      it { should validate_presence_of(:nationality) }
-      it { should validate_presence_of(:birthday) }
-      # let(:author) { build(:author, name: nil) }
-
-      # it 'then not OK' do
-      #   expect(author).not_to be_valid      
-    end
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:nationality) }
+    it { is_expected.to validate_presence_of(:birthday) }
   end
 end
+
+# describe 'Given an author' do
+#   context "when it's valid" do
+#     let(:author) { build(:author) }
+
+#     it 'then OK' do
+#       expect(author).to be_valid
+#     end
+#   end
+
+# let(:author) { build(:author, name: nil) }
+
+# it 'then not OK' do
+#   expect(author).not_to be_valid
